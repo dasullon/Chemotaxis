fb41ef61789981dab8ec3546c0615bb5e6972f46
unknown <dasullon@697-334-Linhir.sf.k12.internal>
2014-09-30 10:49:10 -0700

    added first name ti index.html

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 	Bacteria [] james;
 
-	void setup()   
+	public void setup()   
 	{     
 		size (300,300);  
 		background(0); 
@@ -12,7 +28,7 @@
 
 	}
 
-	void draw()
+	public void draw()
 	{
 		
 	for (int i=0; i< james.length;i++)
@@ -37,7 +53,7 @@
 			y=(int)(Math.random()*299); 
 		}   
 
-	void move()
+	public void move()
 		{
 			if ((int)(Math.random()*2+1)==1){
 				x++;
@@ -55,8 +71,17 @@
 		}
 
 
-	void show(){
+	public void show(){
 		fill(a,b,c);
 		ellipse(x, y, 5, 5);
 		}
 	}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
